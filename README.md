dd097cc0735673fd5ce09015e7b277982e8aeb5a
Abdullah Sahraoui <abdullahsahraoui@MacBook-Pro.cust.communityfibre.co.uk>
2023-10-01 19:54:56 +0100

    add frontend boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# special-dollop
+# Chat application
+
+This is a monorepo project for my own practice using websockets with JS. 
+It will include a backend and a frontend.
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className="flex flex-row w-screen h-screen">This is an interesting div</div>
+		</>
+	);
+}
+
+export default App;
