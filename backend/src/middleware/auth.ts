e83ef8a56cfd2e610372bfb656837fb5309819d3
Abdullah Sahraoui <abdullahsahraoui@MacBook-Pro.cust.communityfibre.co.uk>
2023-11-02 00:06:01 +0000

    remove explicit jwt_secret

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -2,20 +2,24 @@ import { randomUUID } from 'crypto';
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-export const JWT_SECRET = 'testing123'; 
+export const JWT_SECRET = process.env.JWT_SECRET || randomUUID();
 
-const authenticate = (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
 	const authHeader = req.headers['authorization'];
 	const token = authHeader && authHeader.split(' ')[1];
 
 	if (token == null) return res.sendStatus(401);
 
 	jwt.verify(token, JWT_SECRET, (err, user) => {
-		if (err) return res.status(403).json({ success: false, message: err.message, token, expectedToken: JWT_SECRET});
+		if (err)
+			return res
+				.status(403)
+				.json({
+					success: false,
+					message: err.message,
+					token,
+					expectedToken: JWT_SECRET,
+				});
 		req.user = user;
 		next();
 	});
