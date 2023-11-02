7ee3afdb9877d8dcbfe41d7920b794e8602aa165
Abdullah Sahraoui <abdullahsahraoui@MacBook-Pro.cust.communityfibre.co.uk>
2023-11-02 00:23:56 +0000

    add types for jwt-logic functionality

--- types/index.ts
+++ types/index.ts
@@ -1,72 +1,46 @@
-export type DBResponse<T, K> =
-	| {
-			response: T;
-			additionalData?: K;
-	  }
-	| Error;
+import {
+	User,
+	Room,
+	RoomMembers,
+} from '../backend/node_modules/generated/prisma';
 
-export type DBAttribute =
-	| 'UserId'
-	| 'Username'
-	| 'BlockedList'
-	| 'DisplayName'
-	| 'Email'
-	| 'FriendsList'
-	| 'FullName'
-	| 'JoinedRooms'
-	| 'LastSeen'
-	| 'Online'
-	| 'ProfilePicture';
-
-export type UpdateAttributes<T, K extends keyof T> = {
-	[P in K]?: T[P];
+export type ApiResponse<T> = {
+	success: boolean;
+	response: T | Error;
 };
 
-export type UserAttributeMappings = {
-	UserId: User['id'];
-	Username: User['username'];
-	BlockedList: User['blockedList'];
-	DisplayName: User['displayname'];
-	Email: User['email'];
-	FriendsList: User['friendsList'];
-	FullName: User['fullname'];
-	JoinedRooms: User['joinedRooms'];
-	LastSeen: User['lastSeen'];
-	Online: User['online'];
-	ProfilePicture: User['profilePicture'];
-};
+declare global {
+	namespace Express {
+		interface Request {
+			// TODO: type up user to be User object (from prisma generated table)
+			user?: any;
+		}
+	}
+}
+
 
 export interface ServerToClientEvents {
 	kicked_user: (data: User) => void;
 	receive_message: (data: ReceiveMessage) => void;
 }
 export interface ClientToServerEvents {
-	join_room: (user: User, room: Room['id']) => void;
-	leave_room: (user: User, room: Room['id']) => void;
+	auth: (userId: User['id']) => void;
+	user_disconnect: () => void;
+	join_room: (userId: User['id'], room: Room['id']) => void;
+	leave_room: (userId: User['id'], room: Room['id']) => void;
+	create_and_join_room: (userId: User['id'], room: RoomInput) => void;
+	private_message: (recipientId: User['id'], message: string) => void;
 }
 
 export type ReceiveMessage = {
 	message: string;
-	username: string;
+	senderId: string;
 	__createdtime__: string;
 };
 
-export type User = {
-	id: string;
-	username: string;
-	displayname: string;
-	password: string;
-	email: string;
-	friendsList: User[];
-	blockedList: User[];
-	joinedRooms: Room[];
-	online: boolean;
-	lastSeen: string;
-	fullname: string;
-	profilePicture: string;
-	location?: string;
-	preferences?: UserPreferences;
-	socials?: SocialLink;
+export type RoomInput = Omit<Room, 'userId'> & {
+	createdBy?: User;
+	members?: RoomMembers[];
 };
 
 export type UserPreferences = {
@@ -84,16 +58,16 @@ export type SocialPlatform =
 	| 'snapchat'
 	| 'instagram';
 
-export type Room = {
-	id: string;
-	createdBy: User['username'];
-	createdAt: string;
-	name: string;
-	description: string;
-	members: User['id'][] | User['username'][];
-	isPrivate: boolean;
-	typingUsers?: User[];
-};
+// export type Room = {
+// 	id: string;
+// 	createdBy: User['username'];
+// 	createdAt: string;
+// 	name: string;
+// 	description: string;
+// 	members: User['id'][] | User['username'][];
+// 	isPrivate: boolean;
+// 	typingUsers?: User[];
+// };
 
 export type Message = {
 	id: string;
